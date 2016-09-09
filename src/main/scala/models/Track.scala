9b074c674696adf7598a2554788642637787b1b6
Thomas Jakeway <tjakeway92@gmail.com>
2016-09-09 13:11:05 -0400

    made the case class for track model

--- src/main/scala/models/Track.scala
+++ src/main/scala/models/Track.scala
@@ -1,5 +1,22 @@
 package models
 
-class Track {
-
-}
+case class Track(
+                album: AlbumSimple,
+                artists: List[ArtistSimple],
+                available_markets: List[String],
+                disc_number: Int,
+                duration_ms: Int,
+                explicit: Boolean,
+                external_ids: Map[String, String],
+                external_urls: Map[String, String],
+                href: String,
+                id: String,
+                is_playable: Option[Boolean],
+                linked_from: Option[TrackLink],
+                name: String,
+                popularity: Int,
+                preview_url: String,
+                track_number: Int,
+                `type`: String,
+                uri: String
+                )
